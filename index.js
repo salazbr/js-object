c49cbf37c8b7c47d94077455057edd35f3aa7f2e
salazbr <salazbr@oocl.com>
2022-08-23 11:49:55 +0800

    Modify index.js and Add answers for the activity

---
+++ index.js
@@ -0,0 +1,12 @@
+var user = {name: 'John', surname: 'Mike'};
+user.name = 'Peter';
+delete user.name;
+
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+    };
+
+var output = fruit.apple + fruit.pear + fruit.peach;
+
